45b8d80aa09d4db2bfb5dcffd44e4dc4074272f1
Esther Mwaura <esther.mwaura1@student.moringaschool.com>
2026-06-12 02:36:11 -0700

    Made changes to the lab

--- lib/book.py
+++ lib/book.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 
 class Book:
-    def init(self, title, page_count):
+    def __init__(self, title, page_count):
         """Initialize a Book with a title and page count."""
         self.title = title
         self._page_count = None
         self.page_count = page_count
         self._current_page = 1
-
+    
     @property
     def page_count(self):
         return self._page_count
-
+    
     @page_count.setter
     def page_count(self, value):
         if isinstance(value, int):
             self._page_count = value
         else:
             print("page_count must be an integer")
-
+    
     def turn_page(self):
         """Turn to the next page."""
         if self._current_page < self.page_count:
@@ -27,8 +27,3 @@ class Book:
         else:
             print("You've reached the end of the book!")
     
-        
-
-
-    
-        
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,16 +1,15 @@
-#!/usr/bin/env python3
 class Coffee:
-    def init(self, size, price):
+    def __init__(self, size, price):
         """Initialize a Coffee with size and price."""
         self._size = None
         self.size = size  # Use property setter for validation
         self.price = price
-
+    
     @property
     def size(self):
         """Get the size of the coffee."""
         return self._size
-
+    
     @size.setter
     def size(self, value):
         """Set the size with validation."""
@@ -20,7 +19,7 @@ class Coffee:
         else:
             print("size must be Small, Medium, or Large")
             self._size = None
-
+    
     def tip(self):
         """Add a tip to the coffee."""
         print("This coffee is great, here’s a tip!")
